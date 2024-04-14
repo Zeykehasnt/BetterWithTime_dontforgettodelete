5ce1199c1922fd1b58a443740cf4e55cdfff388e
Luna <ratherbelunar@gmail.com>
2024-04-14 14:30:32 -0400

    Unfired pottery now requires a supporting block

--- src/main/java/com/bwt/blocks/UnfiredPotteryBlock.java
+++ src/main/java/com/bwt/blocks/UnfiredPotteryBlock.java
@@ -1,9 +1,14 @@
 package com.bwt.blocks;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.*;
+import net.minecraft.block.enums.RailShape;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
 
 public abstract class UnfiredPotteryBlock extends Block {
     public static final BooleanProperty COOKING = BooleanProperty.of("cooking");
@@ -17,5 +22,18 @@ public abstract class UnfiredPotteryBlock extends Block {
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         super.appendProperties(builder);
         builder.add(COOKING);
+    }
+
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        return world.getBlockState(pos.down()).isSideSolid(world, pos.down(), Direction.UP, SideShapeType.RIGID);
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (!state.canPlaceAt(world, pos)) {
+            return Blocks.AIR.getDefaultState();
+        }
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 }
